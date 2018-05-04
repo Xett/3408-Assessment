6b582c5b176a1d0680bbba48776939f0c60f2898
Xett <3954937+Xett@users.noreply.github.com>
2018-05-04 23:31:34 +1000

    Initial Commit
    Player Model and Movement.
    Beginning implementation of weather system(Rain particle system, Lightning particle system, Spark particle system). Endgame score HUD implementation begun. Day/night system implementation begun.

---
+++ Assets/Scripts/LightTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightTimer : MonoBehaviour
+{
+    public GameObject directionalLight;
+    public float maxGameTime;
+    private static float gameTime;
+    public Text timerText;
+
+    // Use this for initialization
+    void Start ()
+    {
+        if(directionalLight==null)
+        {
+            directionalLight = GameObject.FindWithTag("directionalLight");
+        }
+        gameTime = maxGameTime;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameTime -= Time.deltaTime;
+
+        // Light direction
+        float percentageDayComplete = gameTime / maxGameTime;
+        float xAngle = 180 * percentageDayComplete;
+        Quaternion newRotation = Quaternion.Euler(xAngle, 0, 0);
+        directionalLight.transform.rotation = newRotation;
+
+        timerText.text = "Timer: " + gameTime;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float sensitivityX;
+    public float sensitivityY;
+    public float minimumX = -60f;
+    public float maximumX = 60f;
+    public float minimumY = 360f;
+    public float maximumY = 360f;
+
+    public Camera playerCamera;
+
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+
+    float rotationX = 0f;
+    float rotationY = 0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        rotationX += Input.GetAxis("Mouse Y")*sensitivityX;
+        rotationY += Input.GetAxis("Mouse X")*sensitivityY;
+
+        rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+
+        Movement(h, v);
+        Turning();
+        //Animating(h, v);
+	}
+
+    void Movement(float h, float v)
+    {
+        movement = (transform.forward*v) + (transform.right*h);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        playerRigidbody.MoveRotation(Quaternion.Euler(0, rotationY, 0));
+        playerCamera.transform.rotation = Quaternion.Euler(-rotationX, rotationY, 0);
+    }
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+    }
+}

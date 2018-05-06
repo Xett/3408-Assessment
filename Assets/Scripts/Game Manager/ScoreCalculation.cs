c93e3246f831e72f0dce1337c93d80bc0da83050
JacobJ97 <jacobjackson977@hotmail.com>
2018-05-07 02:45:32 +1000

    adding functionalities for picking up items and [basic] scoring

---
+++ Assets/Scripts/Game Manager/ScoreCalculation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCalculation : MonoBehaviour {
+
+	public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// whenever pickup item enters shed, score increases
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "pickupItem") {
+				score += 10;
+		}
+	}
+
+	// whenever pickup item leaves shed, score decreases
+	void OnTriggerExit(Collider col) {
+		if (col.gameObject.tag == "pickupItem") {
+			score -= 10;
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PickUpItems.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PickUpItems : MonoBehaviour {
+	
+	public bool holdingItem = false;
+	public GameObject pickupItem;
+	public Transform guide;
+	public Camera cam;
+	public float closestDistance = -0f;
+	public List<Collider> TriggerList = new List<Collider>();
+
+	void Start() {
+		cam = Camera.main;
+	}
+		
+	void Update() {
+		//checks for an input press on every frame
+		if (Input.GetKeyDown("return")) {
+			if (holdingItem) {
+				placeDown();
+			}
+			else {
+				pickUp();
+			}
+		}
+
+		//if one or more objects are within a collider, distance between player and object is calculated
+		if (!holdingItem && TriggerList.Any()) {
+			foreach (Collider singlecol in TriggerList) {
+				float distance = Vector3.Distance(cam.transform.position, singlecol.transform.position);
+
+				if (closestDistance == -0f) {
+					closestDistance = distance;
+					pickupItem = singlecol.gameObject;
+				}
+				else {
+					if (distance < closestDistance) {
+						closestDistance = distance;
+						pickupItem = singlecol.gameObject;
+					} 
+				}
+
+				closestDistance = -0f;
+			}
+
+		}
+
+		//
+		if (holdingItem && pickupItem) {
+			pickupItem.transform.position = guide.position;
+		}
+	}
+
+	//when player comes into contact area with object
+	void OnTriggerEnter(Collider col) {
+		if (holdingItem) {
+			return;
+		}
+
+		if (col.gameObject.tag == "pickupItem") {
+			if (!TriggerList.Contains (col)) {
+				TriggerList.Add (col);
+			}
+		}
+	}
+
+	//when player comes out of contact area with object
+	void OnTriggerExit(Collider col) {
+		if (holdingItem) {
+			return;
+		}
+
+		if (col.gameObject.tag == "pickupItem") {
+			if (TriggerList.Contains(col)) {
+				TriggerList.Remove(col);
+			}
+
+			if (col.gameObject == pickupItem) {
+				pickupItem = null;
+			}
+		}
+	}
+		
+	private void pickUp() {
+		if (!pickupItem) {
+			return;
+		}
+
+		//Set object parent to our guide empty object.
+		pickupItem.transform.SetParent(guide);
+
+		//when carrying object, it does not effect any objects
+		pickupItem.GetComponent<Rigidbody> ().isKinematic = true;
+
+		//we apply the same rotation our main object (Camera) has.
+		pickupItem.transform.localRotation = transform.rotation;
+
+		//Reposition transform based on guide
+		pickupItem.transform.position = guide.position;
+
+		//clear closest distance
+		closestDistance = -0f;
+
+		holdingItem = true;
+	}
+
+	private void placeDown() {
+		if (!pickupItem) {
+			return;
+		}
+
+		//regular physics
+		pickupItem.GetComponent<Rigidbody> ().isKinematic = false;
+
+		//Unparent our ball
+		guide.GetChild(0).parent = null;
+
+		holdingItem = false;
+	}
+}

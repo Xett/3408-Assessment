e4588e1746a79f129f79dd7ca4df89070f280c74
Xett <3954937+Xett@users.noreply.github.com>
2018-05-24 08:33:49 +1000

    Script timer fixes
    No more errors.
    -GameManager contains the timer
    -HUDCanvas and Timertext get the gameTime value through the GameManager
    Sun moves through the sky properly too

--- Assets/Scripts/Game Manager/GameTimer.cs
+++ Assets/Scripts/Game Manager/GameTimer.cs
@@ -1,32 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GameTimer : MonoBehaviour
 {
     public float maxGameTime;
-	Text timerString;
     public float gameTime;
     public float percentageDayComplete;
 
     // Use this for initialization
     void Start ()
     {
-		timerString = GetComponent<Text> ();
         gameTime = maxGameTime;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if (gameTime > 0) {
+		if (gameTime > 0)
+        {
 			gameTime -= Time.deltaTime;
-			int mins = Mathf.FloorToInt (gameTime / 60F);
-			int secs = Mathf.FloorToInt (gameTime - mins * 60);
-			string formattedTime = string.Format ("{0:0}:{1:00}", mins, secs);
-			timerString.text = formattedTime;
-			percentageDayComplete = gameTime / maxGameTime;
+			percentageDayComplete = 1 - (gameTime / maxGameTime);
 		}
     }
 }
